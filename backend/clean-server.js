e20dbf438e51fa999271f1edc9ffc14139dd1d96
KChilufya@gmail.com <kchilufya@gmail.com>
2025-08-29 10:34:11 -0400

    Deleted the nested backend folder

--- backend/clean-server.js
+++ backend/clean-server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-echo; "// trigger re-commit" >> clean-server.js
 
 const app = express();
 
